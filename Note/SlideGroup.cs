c737152e25294dbef8c7bd3d3b3aa429648ee0ed
Neskol <hs-neskol@outlook.com>
2023-03-06 01:06:13 -0500

    Slide work comit

--- Note/SlideGroup.cs
+++ Note/SlideGroup.cs
@@ -4,9 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MaiLib.Note
+namespace MaiLib
 {
-    internal class SlideGroup
+    public class SlideGroup : Slide
     {
+        private List<Slide> internalSlides;
+        public SlideGroup()
+        {
+            this.internalSlides = new();
+        }
+
+        public SlideGroup(Note inTake) : base(inTake)
+        {
+            this.internalSlides = new()
+            {
+                (Slide)inTake
+            };
+        }
+
+        public SlideGroup(List<Slide> slideCandidate)
+        {
+            this.internalSlides = new();
+            this.internalSlides.AddRange(slideCandidate);
+        }
+
+        public override string Compose(int format)
+        {
+            return base.Compose(format);
+        }
     }
 }
