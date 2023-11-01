60c1a3dcdf4765c4944cdd39b66ab93e112d370a
Neskol Lu <hs-neskol@outlook.com>
2023-11-02 00:25:47 +0800

    SimaiScanner implementation update

--- Parser/SimaiScanner.cs
+++ Parser/SimaiScanner.cs
@@ -1,29 +1,66 @@
-namespace MaiLib;
-
-public class SimaiScanner
+using static MaiLib.NoteEnum;
+namespace MaiLib
 {
-    public string[]? IncomingChart { get; private set; }
-    public Dictionary<NoteEnum,string> TokenBuffer { get; protected set; }
-    public int LineNum { get; private set; }
-    public int CharNum { get; private set; }
-
-    public SimaiScanner()
+    public class SimaiScanner
     {
-        TokenBuffer = new Dictionary<NoteEnum, string>();
-        LineNum = 0;
-        CharNum = 0;
+        public string[]? IncomingChart { get; private set; }
+        public Dictionary<NoteType, string> TokenSet { get; protected set; }
+        public int LineNum { get; private set; }
+        public int CharNum { get; private set; }
+        public string TokenBuffer { get; private set; }
+
+        public SimaiScanner()
+        {
+            TokenSet = new Dictionary<NoteType, string>();
+            TokenBuffer = "";
+            LineNum = 0;
+            CharNum = 0;
+        }
+
+        public SimaiScanner(string chart)
+        {
+            IncomingChart = chart.Split("\n");
+            TokenSet = new Dictionary<NoteType, string>();
+            TokenBuffer = "";
+            LineNum = 0;
+            CharNum = 0;
+        }
+
+        public void Load(string chart)
+        {
+            IncomingChart = chart.Split("\n");
+        }
+
+        public void Scan()
+        {
+            if (IncomingChart is null) throw new NullReferenceException("The scanner is not provided with valid chart to scan.");
+            LineNum = 0;
+            CharNum = 0;
+            foreach (string line in IncomingChart)
+            {
+                NoteType status = NoteType.RST;
+                foreach (char token in line.ToCharArray())
+                {
+                    switch (token)
+                    {
+                        case '{':
+                            status = NoteType.MEASURE;
+                            break;
+                        case '}':
+                            TokenSet.Add(status, TokenBuffer);
+                            TokenBuffer = "";
+                            break;
+                    }
+                }
+            }
+        }
     }
 
-    public SimaiScanner(string chart)
+    public class NoteStatusNotMatchException : Exception
     {
-        IncomingChart = chart.Split("\n");
-        TokenBuffer = new Dictionary<NoteEnum, string>();
-        LineNum = 0;
-        CharNum = 0;
-    }
-
-    public void Load(string chart)
-    {
-        IncomingChart = chart.Split("\n");
+        public NoteStatusNotMatchException(int lineNum, int charNum, char token, string reason, NoteType expected, NoteType actual) :
+        base($"At Line {lineNum}: Unexpected char {token} at {charNum} is found. Reason: {reason}. Expected Note Tpye: {expected} but actual {actual}.")
+        {
+        }
     }
 }
