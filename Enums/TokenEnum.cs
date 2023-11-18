855e58543474d50f1824a4d61578fbd2683e0da0
Neskol Lu <hs-neskol@outlook.com>
2023-11-19 01:19:43 +0800

    Add Token Enum

--- Enums/TokenEnum.cs
+++ Enums/TokenEnum.cs
@@ -2,8 +2,38 @@ namespace MaiLib;
 
 public class TokenEnum
 {
-    public enum EnumType
+    public enum TokenType
     {
+        LPAREN,
+        RPAREN,
+        LBRACE,
+        RBRACE,
+        LBRACKET,
+        RBRACKET,
+        COMMA,
+        COLON,
+        SHARP,
+        DOT,
+        TOUCHA,
+        TOUCHB,
+        TOUCHC,
+        TOUCHD,
+        TOUCHE,
+        TOUCHF,
+        NUM1,
+        NUM2,
+        NUM3,
+        NUM4,
+        NUM5,
+        NUM6,
+        NUM7,
+        NUM8,
+        NUM9,
+        NUM0,
+        BREAK,
+        EX,
+        HOLD,
+        FIREWORK,
 
     }
 }
